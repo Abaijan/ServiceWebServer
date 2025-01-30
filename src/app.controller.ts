d2da4e0cb11f6f2c463db21e8a6dba9869c0f3ff
Abaijan <fghf09647@gmail.com>
2025-01-30 20:40:32 +0600

    push some updates

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+interface CardProps {
+  id: number;
+  category: string;
+  image: string;
+  title: string;
+}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getCards(): CardProps[]{
+    return this.getCards()
   }
 }
