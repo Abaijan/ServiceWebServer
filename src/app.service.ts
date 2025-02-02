07d3268b62ef98a7ac02195447051dac77bd7ba2
Abaijan <fghf09647@gmail.com>
2025-02-02 22:55:35 +0600

    daily cpmmmit

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,7 +7,6 @@ import { cardMainPage, cardMainPageDocument } from './cards.schema';
 export class CardsService {
     constructor(@InjectModel(cardMainPage.name) private cardModel: Model<cardMainPageDocument>) {}
 
-    // Убираем id из параметров
     async createCard(category: string, image: string, title: string): Promise<HydratedDocument<cardMainPageDocument>> {
         return this.cardModel.create({ category, image, title });
     }
